98081870b4a1461d0245465ae68347a28a942d11
Bennett Gunson <bengunson@gmail.com>
2022-01-14 16:38:34 -0700

    Updates

---
+++ 252f366e-371e47ee65815247b8e3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_site=self.webpackChunkmy_site||[]).push([[228],{2775:function(t,h,v){v.d(h,{FFD:function(){return n}});var a=v(1046);function n(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M7 11h2v2H7v-2zm14-5v14c0 1.1-.9 2-2 2H5a2 2 0 01-2-2l.01-14c0-1.1.88-2 1.99-2h1V2h2v2h8V2h2v2h1c1.1 0 2 .9 2 2zM5 8h14V6H5v2zm14 12V10H5v10h14zm-4-7h2v-2h-2v2zm-4 0h2v-2h-2v2z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-371e47ee65815247b8e3.js.map
