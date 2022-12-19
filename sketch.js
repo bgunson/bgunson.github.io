83f9af4ed2952dc9cbed8c4aaa61cf67643cfeea
Bennett Gunson <bengunson@gmail.com>
2022-12-19 15:33:12 -0700

    Updates

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 let orbitals;
+let thetaM = 1;
+let thetaInterval;
+
 function setup(){
     var canvas = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
     // canvas.parent("sketch");
@@ -12,12 +15,25 @@ function setup(){
     }
 }
 
+function mouseWheel(event) {
+    thetaM = 10;
+    clearInterval(thetaInterval);
+    thetaInterval = setInterval(() => {
+        if (thetaM <= 1) {
+            clearInterval(thetaInterval);
+        } else {
+            thetaM-=0.5;
+        }
+    }, 125);
+    // setTimeout(() => thetaM = 1, 2000);
+}
+
 function draw(){
     translate(-width/2, -height/2);
     delay(10);
     background(0);
     translate(width / 2, height / 2);
-    pointLight(204, 204, 204, 0, 0, 0);
+    pointLight(200, 200, 200, 0, 0, -100);
     pointLight(100, 100, 100, width / 2, height / 2, 100);
     for (let P2JSi = 0; P2JSi < orbitals.length; P2JSi++){
         let o = orbitals[P2JSi];
@@ -29,25 +45,25 @@ class Orbital{
     constructor(){
         this.posY = 0;
         this.minSize = random(5, 16);
-        this.maxSize=random(20, 100);
+        this.maxSize=random(100, 250);
         this.posX=this.posY=0;
         this.rad=this.maxSize;
         this.dRad=random(0.01, 0.15);
         this.radiusX=random((width / 2) - (2 * this.maxSize));
         this.radiusY=random((height / 2) - (2 * this.maxSize));
         this.theta=random(360);
-        this.dTheta=random(0.01, 0.015);
+        this.dTheta=random(0.001, 0.002);
     }
 
     draw(){
-        this.theta+=this.dTheta;
+        this.theta+=this.dTheta*thetaM;
         this.posX=this.radiusX * cos(this.theta);
         this.posY=this.radiusY * sin(this.theta);
         translate(this.posX, this.posY);
-        this.rad+=this.dRad;
-        if(this.rad <= this.minSize || this.rad > this.maxSize) {
-            this.dRad*=-1;
-        }
+        // this.rad+=this.dRad;
+        // if(this.rad <= this.minSize || this.rad > this.maxSize) {
+        //     this.dRad*=-1;
+        // }
         sphere(this.rad);
     }
 
